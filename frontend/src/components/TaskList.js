8e7bb34b6d052197d2b2a29e14222021d08c8483
NitaiEdelberg <nitai.edel@gmail.com>
2026-07-19 17:16:45 +0300

    fix duplication bug, caoused by the carousel

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -8,6 +8,25 @@ function TaskList({ tasks, onToggle, onDelete, onEdit }) {
 
   //duplicate the tasks to keep sliding after task (after testing) - https://blog.logto.io/css-only-infinite-scroll
   const carouselTasks = [...tasks, ...tasks];
+  //so we won't see duplications.
+  if (tasks.length < 4) {
+    return (
+      <div className="carousel-wrapper">
+        <div className="carousel-track no-scroll">
+          {tasks.map(task => (
+            <div className="carousel-slide" key={task.id}>
+              <TaskItem
+                task={task}
+                onToggle={onToggle}
+                onDelete={onDelete}
+                onEdit={onEdit}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="carousel-wrapper">
